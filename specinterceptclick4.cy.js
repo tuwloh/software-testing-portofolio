400634042d09ea019cad3fcd1102b0261d73c8fc
tuwloh <akas.ilham@gmail.com>
2025-08-26 12:26:21 +0700

    second commit

---
+++ specinterceptclick4.cy.js
@@ -0,0 +1,12 @@
+describe('fitur intercept untuk menyelidiki serangan siber', () => {
+  it('intercept click tidak input apapun di kolom username', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('[name="username"]').should('be.empty')
+    cy.get('[name="password"]').should('be.empty')
+    cy.intercept('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/core/i18n/messages').as('login');
+    cy.get('[type="submit"]').click();
+    cy.wait('@login').then((intercept) => {
+        expect(intercept.response.responseTime).to.be.below(22600);
+    })
+  })
+})
